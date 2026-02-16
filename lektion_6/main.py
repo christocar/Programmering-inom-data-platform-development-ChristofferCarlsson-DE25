3c396516c1e7161a3c89d8396c4e3a27c0d5c6f2
CHRISTOFFER CARLSSON <CHRISTOFFER.CARLSSON88@gmail.com>
2026-02-16 17:12:51 +0100

    lektion 6

--- lektion_6/main.py
+++ lektion_6/main.py
@@ -1,1 +1,14 @@
 from fastapi import FastAPI, status
+from psycopg3 import Connect
+from psycopg3 import ConnectionPool
+from psycopg3.type.json import Json
+
+DATABASE_URL = "postgres://Panzword88@localhost:5432/postgres"
+pool = ConnectionPool(DATABASE_URL)
+app = FastAPI(title="lektion-6-test-schema")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
